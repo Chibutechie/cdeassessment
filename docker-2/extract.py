472abfc317aa9895cb8e3792bdd54aa2ec32dec3
Chibu <chibuezeanalyst@gmail.com>
2025-10-03 05:31:27 +0100

    Add files

---
+++ docker-2/extract.py
@@ -0,0 +1,24 @@
+import kaggle
+import pandas as pd
+
+# Authenticate with Kaggle API
+kaggle.api.authenticate()
+
+# Download and unzip the dataset
+kaggle.api.dataset_download_files('carrie1/ecommerce-data', path='.', unzip=True)
+
+# Get dataset metadata
+kaggle.api.dataset_metadata('carrie1/ecommerce-data', path='.')
+
+
+try:
+    df = pd.read_csv('data.csv', encoding='ISO-8859-1') 
+    
+    print("\nFirst 5 rows of the DataFrame:")
+    print(df.head())
+
+    
+except FileNotFoundError:
+    print("Error: The dataset file was not found. Please check the filename in the unzipped folder.")
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
