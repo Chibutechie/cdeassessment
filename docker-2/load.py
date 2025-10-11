f610bd5c674a888640a73380740f808fcef1da84
Chibu <chibuezeanalyst@gmail.com>
2025-10-11 21:23:50 +0100

    Modify files

--- docker-2/load.py
+++ docker-2/load.py
@@ -3,21 +3,18 @@ from sqlalchemy import create_engine
 
 connect = {
     "host": "localhost",
-    "dbname": "elt_db",
+    "dbname": "postgres",
     "user": "postgres",
     "password": "024899",
     "port": 5432
 }
 
 try:  
-    # Create engine
     engine = create_engine(f"postgresql+psycopg2://{connect['user']}:{connect['password']}@{connect['host']}:{connect['port']}/{connect['dbname']}")
     
-    # Read CSV file
     data = pd.read_csv('data.csv', encoding='ISO-8859-1')
     
-    # Load data into PostgreSQL
-    data.to_sql('data', engine, schema='public', if_exists='append', index=False)
+    data.to_sql('data', engine, schema='public', if_exists='replace', index=False)
     
     print(f"Data loaded successfully! {len(data)} rows inserted.")
 
